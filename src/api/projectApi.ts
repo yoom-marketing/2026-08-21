8dc655b8c92b9ceb2d87b75d400ff030571697a2
yoom-marketing <github_demo.trigger.timetechnologies@yoom.fun>
2026-08-21 10:22:40 +0900

    Add project API type and fetch function

--- src/api/projectApi.ts
+++ src/api/projectApi.ts
@@ -1,1 +1,12 @@
-// Project API endpoints
+import httpClient from './httpClient';
+
+export type Project = {
+  id: string;
+  name: string;
+  ownerId: string;
+};
+
+export async function fetchProjects(): Promise<Project[]> {
+  const res = await httpClient.get<Project[]>('/projects');
+  return res.data;
+}
