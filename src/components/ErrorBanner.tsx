a69b704760dc2e39818d11a5b9de0c13b3b0723a
yoom-marketing <github_demo.trigger.timetechnologies@yoom.fun>
2026-08-21 10:24:15 +0900

    Add ErrorBanner component for displaying error messages

--- src/components/ErrorBanner.tsx
+++ src/components/ErrorBanner.tsx
@@ -1,1 +1,13 @@
-// Error Banner component
+import React from 'react';
+
+type Props = {
+  message: string;
+};
+
+export const ErrorBanner: React.FC<Props> = ({ message }) => {
+  return (
+    <div style={{ backgroundColor: '#fee2e2', color: '#b91c1c', padding: 8 }}>
+      {message}
+    </div>
+  );
+};
