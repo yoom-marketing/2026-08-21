1ca54ec56f79f34421443a67c1ad9b5f1a39f56d
yoom-marketing <github_demo.trigger.timetechnologies@yoom.fun>
2026-08-21 10:22:22 +0900

    Implement Todo API with fetch and toggle functions

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,1 +1,18 @@
-// Todo API endpoints
+import httpClient from './httpClient';
+
+export type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+  userId: string;
+};
+
+export async function fetchTodos(): Promise<Todo[]> {
+  const res = await httpClient.get<Todo[]>('/todos');
+  return res.data;
+}
+
+export async function toggleTodo(id: string, completed: boolean): Promise<Todo> {
+  const res = await httpClient.patch<Todo>(`/todos/${id}`, { completed });
+  return res.data;
+}
