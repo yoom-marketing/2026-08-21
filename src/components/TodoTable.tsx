81c38e9a182d8adf1955be38e216ccc22aeb48b7
yoom-marketing <github_demo.trigger.timetechnologies@yoom.fun>
2026-08-21 10:27:01 +0900

    Implement TodoTable component with toggle functionality

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -1,1 +1,38 @@
-// Todo Table component
+import React from 'react';
+import type { Todo } from '../api/todoApi';
+
+type Props = {
+  todos: Todo[];
+  onToggle: (id: string, completed: boolean) => void;
+};
+
+export const TodoTable: React.FC<Props> = ({ todos, onToggle }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>タイトル</th>
+          <th>完了</th>
+          <th>ユーザーID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map((t) => (
+          <tr key={t.id}>
+            <td>{t.id}</td>
+            <td>{t.title}</td>
+            <td>
+              <input
+                type="checkbox"
+                checked={t.completed}
+                onChange={(e) => onToggle(t.id, e.target.checked)}
+              />
+            </td>
+            <td>{t.userId}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
